9e78898258f6b7a57b17257e2b07b1eb71657b52
Mario Fentler <mfentler@student.tgm.ac.at>
2018-10-17 10:47:22 +0200

    Done mixtaping the two tutorials

--- src/main/python/client/client.py
+++ src/main/python/client/client.py
@@ -1,79 +1,94 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask,request, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 app = Flask(__name__)
 api = Api(app)
 
-users = [
-    {
-        "id":0,
-        "name": "Mario",
-        "email":"mfentler@student.tgm.ac.at"
-    },
-    {
-        "id":1,
-        "name":"Max",
-        "email":"mmuster@gmail.com"
-    }
+parser = reqparse.RequestParser()
+parser.add_argument('user')
 
-]
+
+class User_DB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    picture = db.Column(db.String(400))
+
+    def __init__(self, username, email,picture):
+        self.username = username
+        self.email = email
+        self.picture = picture
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('username', 'email','picture')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
 
 class User(Resource):
 
-    def get(self, name="nix"):
-        for user in users:
-            if(name=="nix"):
-                return user,200
-            elif(name==user["name"]):
-                return user, 200
-        return "User not found",404
-    def post(self,name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("email")
-        args = parser.parse_args()
+    def get(self, user_id):
+        user = User.query.get(user_id)
+        return user_schema.jsonify(user)
 
-        userCounter = 0
+    def delete(self, user_id):
+        user = User.query.get(user_id)
+        db.session.delete(user)
+        db.session.commit()
 
-        for user in users:
-            userCounter += 1
-            if(name == user["name"]):
-                return "User with name {} already exists".format(name),400
+        return user_schema.jsonify(user)
 
-        user = {
-            "id": userCounter,
-            "name":name,
-            "email":args["email"]
-        }
-        users.append(user)
-        return user,201
+    def put(self, user_id):
+        user = User.query.get(user_id)
+        username = request.json['username']
+        email = request.json['email']
 
-    def put(self,name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("email")
-        args = parser.parse_args()
+        user.email = email
+        user.username = username
 
-        userCounter = 0
+        db.session.commit()
+        return user_schema.jsonify(user)
 
-        for user in users:
-            userCounter += 1
-            if (name == user["name"]):
-                user["email"] = args["email"]
-                return user,200
+class UserList(Resource):
 
-        user = {
-            "id": userCounter,
-            "name": name,
-            "email": args["email"]
-        }
-        users.append(user)
-        return user, 201
+    def get(self):
+        all_users = User.query.all()
+        result = users_schema.dump(all_users)
+        return jsonify(result.data)
 
-    def delete(self,name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name),200
+    def post(self):
+        username = request.json['username']
+        email = request.json['email']
+        picture = request.json['picture']
+
+        print(username)
+        new_user = User_DB(username, email, picture)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify(new_user)
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(UserList, '/user')
+api.add_resource(User, '/user/<user_id>')
+# /users
+# /users/<id>
 
 
-if __name__=='__main__':
-    api.add_resource(User,"/user/<string:name>")
+
+if __name__ == '__main__':
     app.run(debug=True)
--- src/main/python/server/server.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-
-app = Flask(__name__)
-api = Api(app)
