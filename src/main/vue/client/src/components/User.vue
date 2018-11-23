20a53f7ccc6133084e30918b8a861ccbfe59099b
Mario Fentler <mfentler@student.tgm.ac.at>
2018-11-23 17:03:59 +0100

    Error at connection vue with REST

--- src/main/vue/client/src/components/User.vue
+++ src/main/vue/client/src/components/User.vue
@@ -5,12 +5,28 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'User',
   data () {
     return {
-      msg: 'Hello!'
+      msg: ''
     }
+  },
+  methods: {
+    getMessage () {
+      axios.get('localhost:5000/user')
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          this.msg = error
+        })
+    }
+  },
+  created () {
+    this.getMessage()
   }
 }
 </script>
